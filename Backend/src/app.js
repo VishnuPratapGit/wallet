9acceccc7e86d622a0e398c2c80d934634a35960
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2025-02-06 19:58:29 +0530

    bug fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(
 
 // app.options("*", cors());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
