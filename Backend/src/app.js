723f4fbfca19c71e33b16b33f1e8176610eb8902
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2025-02-06 20:36:42 +0530

    remove path

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -12,10 +11,6 @@ app.use(
     credentials: true,
   })
 );
-
-// app.options("*", cors());
-
-app.use(express.static("../frontend/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -26,9 +21,5 @@ import accountRouter from "./routes/account.routes.js";
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/account", accountRouter);
-
-app.get("*", (req, res) => {
-  res.sendFile("index.html", { root: "../frontend/dist" });
-});
 
 export { app };
