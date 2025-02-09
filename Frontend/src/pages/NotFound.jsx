fc3f865ab475bfa13652a976356b59904c70907d
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2025-02-10 01:01:46 +0530

    improve NotFound Route

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Undo2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="flex flex-col gap-5">
       <h1>Route not found</h1>
-      <Link to="/" className="mt-2">
-        <button>Go T0 Home</button>
-      </Link>
+
+      <button
+        onClick={() => navigate(-1)}
+        className="flex justify-center items-center gap-2 w-max"
+      >
+        <Undo2 size={20} /> {"Back To Home"}
+      </button>
     </div>
   );
 };
